021db90e86b41b6827f477cb5f309390563d6d7d
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2023-12-07 18:20:24 -0300

    Add files via upload

--- contatocon.php
+++ contatocon.php
@@ -13,20 +13,6 @@ $telefone = mysqli_real_escape_string($conn, $POST['telefone']);
 $email = mysqli_real_escape_string($conn, $POST['email']);
 $mensagem = mysqli_real_escape_string($conn, $POST['mensagem']);
 
-$query = "select nome from nome where nome = '{$nome}' and telefone = '{$telefone}' and email = ('{$email}' and mensagem ='{$mensagem}')";
-
-$result = mysqli_query($conn, $query);
-
-$row = mysqli_num_rows($result);
-
-if($row == 1){
-   $_SESSION['nome'] = $email;
-   header('Location: contato.php');
-   exit();
-}else{
-   $_SESSION['nao_autenicado'] = true;
-   header('Location: contato.php');
-   exit();
-}
+$query = "INSERT into cadastro values" (nome,telefone,email,mensagem) = '{$nome,$telefone,$email,$mensagem}';
 
 ?>
