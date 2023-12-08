4cc38a183c4d3687621c0993191a7fc1563d3098
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2023-12-08 14:31:38 -0300

    Add files via upload

--- contatocon.php
+++ contatocon.php
@@ -10,7 +10,7 @@ $mensagem = filter_input(INPUT_POST, 'mensagem');
 //echo "Nome: $nome <br>";
 //echo "E-mail: $email <br>";
 
-$result_usuario = "INSERT INTO contato (nome, telefone, email, mensagem, created) VALUES ('$nome', '$telefone', '$email', '$mensagem', NOW())";
+$result_usuario = "INSERT INTO contato (nome, telefone, email, mensagem) VALUES ('$nome', '$telefone', '$email', '$mensagem')";
 $resultado_usuario = mysqli_query($con, $result_usuario);
 
 if(mysqli_insert_id($conn)){
